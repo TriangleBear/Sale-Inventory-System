473e5a0e11366578eb58dbd24bd7fbbfdfc36d02
TriangleBear <kakanini31@gmail.com>
2024-06-14 16:26:19 +0800

    Update controller.py

--- Sale-Inventory-System/controller.py
+++ Sale-Inventory-System/controller.py
@@ -1,23 +1,28 @@
 import sys
-sys.path.append("..")
-from Model.model import Model
+from Model.model import User, Registration
 from View.view import View
 
 class Controller:
     def __init__(self):
-        self.model = Model()
         self.view = View(self)
 
     def main(self):
         self.view.main()
-    
-    def register(self, data:list):
-        self.model.registerUser(data)
-        return 0
 
-    def checkInput(self,username, password)->str:
-        value = self.model.getLevelOfAccess(username,password)
-        return value
+    def register(self, number, access, email, username, password):
+        Registration.create_user(number, access, username, password, email)
+
+    def checkInput(self, username, password):
+        user = User.check_username(username)
+        stored_password = User.get_password(username)
+        if user is not None:  # Assuming 'password' is the column name in the database
+            if User.check_password(stored_password, password):
+                return user.get('user_type')  # Assuming 'user_type' is the column name in the database
+            else:
+                return "Incorrect password"
+        else:
+            return "No such user was found"
+
 
 if __name__ == '__main__':
     controller = Controller()
