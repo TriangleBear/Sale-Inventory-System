0607047856a16369a7ed3f2a8720dcba35eeda7d
TriangleBear <kakanini31@gmail.com>
2024-06-15 21:08:01 +0800

    Update Database.py
    Changed to py class

--- Sale-Inventory-System/Utils/Database.py
+++ Sale-Inventory-System/Utils/Database.py
@@ -1,18 +1,15 @@
 import pymysql
 import pymysql.cursors
-import json
+from Utils.credentials import Credentials
 from contextlib import contextmanager
 
-# Load credentials once to avoid repeated file I/O operations
-with open("D:\Programming\CS 304\Sale-Inventory-System\Sale-Inventory-System\Model\credentials.json") as f:
-    credentials = json.load(f)
 
 @contextmanager
 def get_db_connection():
     vivdb = pymysql.connect(
-        host=credentials["host"],
-        user=credentials["username"],
-        password=credentials["password"],
+        host=Credentials.host,
+        user=Credentials.user,
+        password=Credentials.password,
         db="viviandbTEST",
         port=22577,
         cursorclass=pymysql.cursors.DictCursor,
