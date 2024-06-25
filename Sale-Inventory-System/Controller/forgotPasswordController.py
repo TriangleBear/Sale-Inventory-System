7c0fef7fc816813ca6d171c09f9c0338491d8a91
Kuromibo <91880111+Kuromibo@users.noreply.github.com>
2024-06-25 12:55:06 +0800

    login and forgot password fully functional

--- Sale-Inventory-System/Controller/forgotPasswordController.py
+++ Sale-Inventory-System/Controller/forgotPasswordController.py
@@ -10,11 +10,17 @@ class ForgotPasswordController:
         self.view.main()
 
     def update_password(self,data:list):
-        pass
+        change_pass = ForgotPasswordModel(new_password=data[0],
+                                          user_id=data[2])
+        change_pass.update_password()
 
     def checkPassInput(self,data:list):
-        password_model = ForgotPasswordModel()
-        pass
+        password_model = ForgotPasswordModel(user_id=data[2],
+                                             provided_email=data[4],
+                                             provided_username=data[3],
+                                             new_password=data[0],
+                                             confirm_password=data[1])
+        return password_model.confirm_password_update()
 
     def checkAccountInput(self, data:list):
         email_model = ForgotPasswordModel(provided_username=data[0]
@@ -22,7 +28,10 @@ class ForgotPasswordController:
         noAccount = email_model.check_account_existence()
         
         if noAccount == 0: 
-            return [email_model.provided_username,email_model.provided_email,email_model.get_forgot_password_otp()]
+            return [email_model.get_user_id(),
+                    email_model.provided_username,
+                    email_model.provided_email,
+                    email_model.get_forgot_password_otp()]
         else: 
             return noAccount
     
@@ -34,4 +43,4 @@ class ForgotPasswordController:
 
 
     def user_otp_verification(self, user_data:list):
-        Functions.send_otp_email(user_data[1], user_data[2])
+        Functions.send_otp_email(user_data[2], user_data[3])
