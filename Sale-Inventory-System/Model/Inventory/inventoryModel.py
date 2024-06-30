978322307b7a1142ae506670b054e00eb3276ece
TriangleBear <kakanini31@gmail.com>
2024-06-30 20:21:40 +0800

    Implemented get data and table column on database for inventory

--- Sale-Inventory-System/Model/Inventory/inventoryModel.py
+++ Sale-Inventory-System/Model/Inventory/inventoryModel.py
@@ -18,6 +18,16 @@ class InventoryModel:
                 result = cursor.fetchall()
             connection.close()
         return result
+    
+    def get_recipe_column_names(self):
+        with Database.get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """SELECT * FROM Recipes LIMIT 0"""  # Modified to limit the results to 0 to avoid fetching data
+                cursor.execute(sql)
+                # Extracting column names from the cursor's description attribute
+                column_names = [desc[0] for desc in cursor.description]
+            connection.close()
+        return column_names
 
     def get_product_on_database(self):
         with Database.get_db_connection() as connection:
@@ -28,6 +38,16 @@ class InventoryModel:
             connection.close()
         return result
     
+    def get_product_column_names(self):
+        with Database.get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """SELECT * FROM Product LIMIT 0"""  # Modified to limit the results to 0 to avoid fetching data
+                cursor.execute(sql)
+                # Extracting column names from the cursor's description attribute
+                column_names = [desc[0] for desc in cursor.description]
+            connection.close()
+        return column_names
+    
     def get_items_on_database(self):
         with Database.get_db_connection() as connection:
             with connection.cursor() as cursor:
@@ -36,3 +56,32 @@ class InventoryModel:
                 result = cursor.fetchall()
             connection.close()
         return result
+    
+    def get_items_column_names(self):
+        with Database.get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """SELECT * FROM Items LIMIT 0"""  # Modified to limit the results to 0 to avoid fetching data
+                cursor.execute(sql)
+                # Extracting column names from the cursor's description attribute
+                column_names = [desc[0] for desc in cursor.description]
+            connection.close()
+        return column_names
+    
+    def get_supplies_on_database(self):
+        with Database.get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """SELECT * FROM Supply"""
+                cursor.execute(sql)
+                result = cursor.fetchall()
+            connection.close()
+        return result
+    
+    def get_supplies_column_names(self):
+        with Database.get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = """SELECT * FROM Supply LIMIT 0""" # Modified to limit the results to 0 to avoid fetching data
+                cursor.execute(sql)
+                # Extracting column names from the cursor's description attribute
+                column_names = [desc[0] for desc in cursor.description]
+            connection.close()
+        return column_names
