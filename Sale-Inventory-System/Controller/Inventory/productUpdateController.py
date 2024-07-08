b4615927e02ee47b92e80414b6a2232df958d043
TriangleBear <kakanini31@gmail.com>
2024-07-08 11:32:21 +0800

    Update productUpdateController.py

--- Sale-Inventory-System/Controller/Inventory/productUpdateController.py
+++ Sale-Inventory-System/Controller/Inventory/productUpdateController.py
@@ -1,3 +1,10 @@
+from View import ProductUpdateView
 class ProductUpdateController:
-    def __init__(self):
-        pass
+    def __init__(self, managerController, product_data, master):
+        self.mC = managerController
+        self.master = master
+        self.product_data = product_data
+        self.view = ProductUpdateView(self, self.master, product_data=self.product_data)
+
+    def main(self):
+        self.view.main()
