38950f5e949d2c961403a1d33d5867ef9813fcc0
TriangleBear <kakanini31@gmail.com>
2024-07-12 22:35:15 +0800

    Assets to be used

--- Sale-Inventory-System/Controller/HelpAbout/userManualController.py
+++ Sale-Inventory-System/Controller/HelpAbout/userManualController.py
@@ -1,9 +1,10 @@
 from View import UserManualView
+import os
 class UserManualController:
     def __init__(self,managerController,master):
         self.mC = managerController
         self.master = master
-        self.file_path = r"Sale-Inventory-System\Controller\HelpAbout\User-manual-pero-di-pa-tapos-pero-pwede-na-muna.pdf"
+        self.file_path = ("\\Group-6-User-Manual.pdf")
         self.view = UserManualView(self,self.master,self.file_path)
 
     def main(self):
