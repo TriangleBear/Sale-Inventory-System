a53237944098fb655efb6e9e293337a579dbb5d6
TriangleBear <kakanini31@gmail.com>
2025-01-27 00:54:13 +0800

    Update securityModel.py
    Fix for #22

--- Sale-Inventory-System/Model/Security/securityModel.py
+++ Sale-Inventory-System/Model/Security/securityModel.py
@@ -7,32 +7,32 @@ class SecurityModel:
     
     def log_user_activity(self):
         with Database.get_db_connection() as connection:
-            with connection.cursor() as cursor:
+            cursor = connection.cursor()
                 
-                sql = """INSERT INTO UserActivity (user_id, user_log, log_date) VALUES (%s, %s, %s)"""
-                cursor.execute(sql, (self.userActivityData[0], self.userActivityData[1], self.userActivityData[2]))
-            connection.commit()
-            connection.close()
+            sql = """INSERT INTO UserActivity (user_id, user_log, log_date) VALUES (%s, %s, %s)"""
+            cursor.execute(sql, (self.userActivityData[0], self.userActivityData[1], self.userActivityData[2]))
+        connection.commit()
+        connection.close()
 
     def fetch_data_from_user_activity(self):
         with Database.get_db_connection() as connection:
-            with connection.cursor() as cursor:
-                sql = """SELECT * FROM UserActivity"""
-                cursor.execute(sql)
-                result = cursor.fetchall()
-            connection.close()
+            cursor = connection.cursor()
+            sql = """SELECT * FROM UserActivity"""
+            cursor.execute(sql)
+            result = cursor.fetchall()
+        connection.close()
         return result
     
     def search_data(self):
         with Database.get_db_connection() as connection:
-            with connection.cursor() as cursor:
-                # Modify the SQL query to search in relevant fields. Here, it searches in the 'user_log' field.
-                # Use '%' wildcards for partial matches. Adjust the field name as per your database schema.
-                sql = """SELECT * FROM UserActivity WHERE log_id LIKE %s OR user_id LIKE %s OR user_log LIKE %s OR log_date LIKE %s"""
-                search_pattern = f"%{self.search_query}%"
-                cursor.execute(sql, (search_pattern,search_pattern,search_pattern,search_pattern))
-                result = cursor.fetchall()
-            connection.close()
+            cursor = connection.cursor()
+            # Modify the SQL query to search in relevant fields. Here, it searches in the 'user_log' field.
+            # Use '%' wildcards for partial matches. Adjust the field name as per your database schema.
+            sql = """SELECT * FROM UserActivity WHERE log_id LIKE %s OR user_id LIKE %s OR user_log LIKE %s OR log_date LIKE %s"""
+            search_pattern = f"%{self.search_query}%"
+            cursor.execute(sql, (search_pattern,search_pattern,search_pattern,search_pattern))
+            result = cursor.fetchall()
+        connection.close()
         return result
     
 
